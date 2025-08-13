f14d7822af2e2833eab3bc712fdff945dbe30a5b
EnzoPB <enzo.puiroux@mail.com>
2025-08-13 17:19:45 +0200

    wip: status table

--- plugins/widgets/status_table/__init__.py
+++ plugins/widgets/status_table/__init__.py
@@ -20,12 +20,23 @@ class StatusTableWidget(BaseWidget):
         ]),
     }
 
+    colors = [
+        (255, 0, 0),
+        (255, 255, 0),
+        (255, 130, 130),
+        (255, 0, 255),
+        (130, 0, 0),
+        (0, 0, 255),
+        (255, 200, 200),
+        (0, 130, 130),
+    ]
+
     def __init__(self, *args):
         super(StatusTableWidget, self).__init__(*args)
         rows = ceil(len(self.config['items'])/self.config['cols'])
         print(rows, self.config['cols'])
 
-        with dpg.table(parent=self.window, header_row=False, borders_innerV=True, borders_innerH=True):
+        with dpg.table(parent=self.window, header_row=False, borders_innerV=True, borders_innerH=True) as table:
             for i in range(self.config['cols']):
                 dpg.add_table_column()
             index = 0
@@ -34,7 +45,8 @@ class StatusTableWidget(BaseWidget):
                     for j in range(self.config['cols']):
                         with dpg.table_cell():
                             if index < len(self.config['items']):
-                                dpg.add_text(f'{i} {j} {index}')
+                                dpg.highlight_table_cell(table, i, j, [*self.colors[index], 100])
+                                dpg.add_text(self.config['items'][index]['label'])
                                 index += 1
 
 
