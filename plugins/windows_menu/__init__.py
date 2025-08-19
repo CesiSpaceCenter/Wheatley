2a21004c8af36d6440982357f737fe6bce5e208b
EnzoPB <enzo.puiroux@mail.com>
2025-08-19 17:28:45 +0200

    feat: light mode

--- plugins/windows_menu/__init__.py
+++ plugins/windows_menu/__init__.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 from plugins.base_plugin import BasePlugin
 from utils import get_widget
+from dearpygui_ext.themes import create_theme_imgui_light, create_theme_imgui_dark
 
 
 class WindowsMenu(BasePlugin):
@@ -14,3 +15,12 @@ class WindowsMenu(BasePlugin):
 
             dpg.add_menu_item(label='Hide title bar', callback=change_window_parameter, user_data='no_title_bar', check=True)
             dpg.add_menu_item(label='No background', callback=change_window_parameter, user_data='no_background', check=True)
+
+            def toggle_theme(_, value):
+                if value:
+                    theme = create_theme_imgui_light()
+                else:
+                    theme = create_theme_imgui_dark()
+                dpg.bind_theme(theme)
+
+            dpg.add_menu_item(label='Light theme', callback=toggle_theme, check=True)
