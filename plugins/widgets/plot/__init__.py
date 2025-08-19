47002b71ab336c518d3ed27d5a1d26e8b9129a52
EnzoPB <enzo.puiroux@mail.com>
2025-08-19 17:28:30 +0200

    feat: refactor plots, different x for each series

--- plugins/widgets/plot/__init__.py
+++ plugins/widgets/plot/__init__.py
@@ -1,16 +1,19 @@
 import dearpygui.dearpygui as dpg
-from plugins.base_widget import BaseWidget, WidgetConfigItem, WidgetConfigGroup
-from plugins.data_store import DataStore
-from plugins.widget_config import DataPoint
+from plugins.base_widget import BaseWidget, Types
+from plugins.data import Data
 
 
 class PlotWidget(BaseWidget):
     name = 'Plot'
 
     config_definition = {
-        'data_point_x': WidgetConfigItem(DataPoint),
-        'data_point_y': WidgetConfigItem(list[WidgetConfigItem(DataPoint)])
+        'series': Types.List(Types.Group({
+            'x': Types.DataPoint(),
+            'y': Types.DataPoint()
+        }))
     }
+
+    reload = False
 
     def __init__(self, *args):
         super(PlotWidget, self).__init__(*args)
@@ -20,18 +23,19 @@ class PlotWidget(BaseWidget):
                 dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 0)
             dpg.bind_item_theme(self.window, container_theme)
 
+
         self.plot = dpg.add_plot(parent=self.window, width=-1, height=-1)
         dpg.add_plot_legend(parent=self.plot)
 
-        print('data_point_y for plot', self.config['data_point_y'])
         # horizontal axis
-        self.x_axis = dpg.add_plot_axis(axis=dpg.mvXAxis, parent=self.plot, label=self.config['data_point_x'])
+        self.x_axis = dpg.add_plot_axis(axis=dpg.mvXAxis, parent=self.plot)
 
         self.series = {}  # dict that will store every dpg's Series
         self.y_axis = {}  # dict that will store every dpg's YAxis
 
-        for data_point_id in self.config['data_point_y']:
-            data_point = DataStore.plugin.dictionary[data_point_id]  # get the datapoint config from the datastore
+        # create the axis
+        for ser in self.config['series']:
+            data_point = Data.plugin.dictionary[ser['y']]  # get the datapoint config from the datastore
 
             # every y axis represents a unit
             if data_point.unit not in self.y_axis:
@@ -40,29 +44,36 @@ class PlotWidget(BaseWidget):
                     raise Exception('Reached limit of 3 y axis')
                 axis = [dpg.mvYAxis, dpg.mvYAxis2, dpg.mvYAxis3][len(self.y_axis)]
                 # create the y_axis
-                self.y_axis[data_point.unit] = dpg.add_plot_axis(axis=axis, parent=self.plot, label=data_point.unit, auto_fit=True)
+                self.y_axis[data_point.unit] = dpg.add_plot_axis(axis=axis, parent=self.plot, label=data_point.unit)
 
             # create the series
-            self.series[data_point_id] = dpg.add_line_series(
+            self.series[ser['y']] = dpg.add_line_series(
                 parent=self.y_axis[data_point.unit],
-                label=data_point.name,
+                label=data_point.name + (f' ({data_point.unit})' if data_point.unit else ''),
                 x=[],
                 y=[]
             )
+        self.reload = True
 
     def render(self):
-        # get the datapoit's data from the datastore
-        data = DataStore.plugin.data
-        if self.config['data_point_x'] == '':  # ignore if no datapoint for the x axis has been configured
+        if not Data.plugin.has_changed and not self.reload:
             return
 
-        data_x = data[self.config['data_point_x']]
-        if len(data_x) == 0:  # ignore if there is no data yet
+        # get the datapoit's data from the datastore
+        data = Data.plugin.data
+        if self.config['series'] == '':  # ignore if no datapoint has been configured
             return
+
+        Data.plugin.has_changed = False
+        self.reload = False
 
         # update the axis limits to fit with new the data
-        dpg.set_axis_limits(self.x_axis, data_x[-1] - 30, data_x[-1])
-        for series_name, ser in self.series.items():
-            if len(data[series_name]) == 0:  # ignore if there is no data yet
+        #dpg.set_axis_limits(self.x_axis, data_x[0] - 30, data_x[-1])
+        for ser in self.config['series']:
+            if len(data[ser['x']]) == 0 or len(data[ser['x']]) == 0:  # ignore if there is no data yet
                 continue
-            dpg.set_value(ser, [data_x, data[series_name]])  # update the series with the new x and y data
+
+            data_x = data[ser['x']]
+            data_y = data[ser['y']]
+
+            dpg.set_value(self.series[ser['y']], [data_x, data_y])  # update the series with the new x and y data
