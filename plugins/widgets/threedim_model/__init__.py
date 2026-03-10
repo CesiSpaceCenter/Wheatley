7e0ece9c25458c7b4a7a41c26a636e7bd2397f9f
EnzoPB <enzo.puiroux@mail.com>
2026-03-10 17:14:53 +0100

    wip: 3d model widget

--- plugins/widgets/threedim_model/__init__.py
+++ plugins/widgets/threedim_model/__init__.py
@@ -1,14 +1,23 @@
 
-import os
-os.environ["PYOPENGL_PLATFORM"] = "egl"
+import time
+from typing import Optional
 import dearpygui.dearpygui as dpg
 
 from plugins.base_widget import BaseWidget
 from plugins.config_ui import config_types
 from plugins.data import Data
+import logging
+logging.getLogger('moderngl_window').setLevel(logging.INFO)
 
-import pyrender
-import trimesh
+import math
+from pathlib import Path
+from pyglm import glm
+import moderngl
+import moderngl_window as mglw
+from moderngl_window.scene import Scene
+from moderngl_window.context.headless import Window as HeadlessWindow
+from moderngl_window.resources import scenes
+from moderngl_window.meta import SceneDescription
 import numpy as np
 
 
@@ -18,8 +27,21 @@ class ThreeDimModel(BaseWidget):
     config_definition = {
         'orientation_x': config_types.DataPoint(),
         'orientation_y': config_types.DataPoint(),
-        'orientation_z': config_types.DataPoint()
+        'orientation_z': config_types.DataPoint(),
+        'model': config_types.File(),
+        'max_framerate': config_types.Int(default=60),
+        'distance': config_types.Float(default=1),
+        'resolution': config_types.Group({
+            'width': config_types.Int(default=512),
+            'height': config_types.Int(default=512)
+        })
     }
+
+    wnd: Optional[HeadlessWindow] = None
+    scene: Optional[mglw.scene.Scene] = None
+    proj: Optional[glm.mat4x4] = None
+    camera: Optional[glm.mat4x4] = None
+    fbo: Optional[moderngl.Framebuffer] = None
 
     def __init__(self, *args):
         super(ThreeDimModel, self).__init__(*args)
@@ -33,50 +55,49 @@ class ThreeDimModel(BaseWidget):
         if self.config['orientation_z'] == '':
             return"""
 
+        h = self.config['resolution']['height']
+        w = self.config['resolution']['width']
         with dpg.texture_registry():
             self.texture = dpg.add_raw_texture(
-                width=512,
-                height=512,
-                default_value=[0] * 512 * 512,  # fill with 0 (black pixels)
+                width=w,
+                height=h,
+                default_value=[0] * w * h,  # fill with 0 (black pixels)
                 format=dpg.mvFormat_Float_rgb
             )
-        self.image = dpg.add_image(parent=self.window, texture_tag=self.texture, width=512, height=512)
-
-        self.logger.info('loading model')
-        mesh = trimesh.load("model.gltf")
-        mesh = pyrender.Mesh.from_trimesh(list(mesh.geometry.values()))
+        self.image = dpg.add_image(parent=self.window, texture_tag=self.texture, width=w, height=h)
 
 
-        self.logger.info('creating scene')
-        self.scene = pyrender.Scene()
+    def init_3d(self):
+        h = self.config['resolution']['height']
+        w = self.config['resolution']['width']
 
-        pose = trimesh.transformations.rotation_matrix(
-            np.radians(-90),  # angle
-            [1, 0, 0]  # axe
+        self.wnd = HeadlessWindow(size=(w, h))
+        mglw.activate_context(ctx=self.wnd.ctx)
+
+        self.logger.info(f'Using model {self.config['model']}')
+
+        resource_dir = Path(self.config['model']).parent.resolve()
+        mglw.resources.register_scene_dir(resource_dir)
+        self.scene = scenes.load(SceneDescription(path=Path(self.config['model']).name))
+
+        center = self.scene.get_center()
+        self.proj = glm.perspective(glm.radians(75.0), w / h, 0.1, 1000.0)
+        self.camera = glm.lookAt(
+            center + glm.vec3(math.sin(90) * self.scene.diagonal_size*self.config['distance'], 0.0, self.scene.diagonal_size*self.config['distance']),
+            center,
+            glm.vec3(0.0, 1.0, 0.0),
         )
-        self.scene.add(mesh, pose=pose)
 
-
-        self.logger.info('creating camera')
-        camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0)
-        camera_pose = np.array([
-            [0, 0, 1, 3],  # position caméra
-            [0, 1, 0, 1],
-            [-1, 0, 0, 0],
-            [0, 0, 0, 1],
-        ])
-        self.scene.add(camera, pose=camera_pose)
-
-        light = pyrender.DirectionalLight(color=np.ones(3), intensity=3.0)
-        self.scene.add(light, pose=camera_pose)
-
-
-        self.logger.info('creating renderer')
-        self.renderer = pyrender.OffscreenRenderer(512, 512)
-
+        self.fbo = self.wnd.ctx.framebuffer(
+            color_attachments=[self.wnd.ctx.texture((w, h), 3)],
+            depth_attachment=self.wnd.ctx.depth_renderbuffer((w, h)),
+        )
 
     i = 0
+    last_frame_t = 0
     def render(self):
+        if self.config['model'] == '':
+            return
         """if self.config['orientation_x'] == '':
             return
         if self.config['orientation_y'] == '':
@@ -88,20 +109,34 @@ class ThreeDimModel(BaseWidget):
         orientation_y = Data.plugin.dictionary[self.config['orientation_y']]
         orientation_z = Data.plugin.dictionary[self.config['orientation_z']]"""
 
-        angle = self.i * 2 * np.pi / 60
-        self.i += 1
+        if time.monotonic() - self.last_frame_t < 1/self.config['max_framerate']:
+            return
+        self.last_frame_t = time.monotonic()
 
-        pose = trimesh.transformations.rotation_matrix(
-            angle,
-            [0, 0, 1]
+        if self.wnd is None:
+            self.init_3d()
+
+        self.i += 1
+        rx = glm.rotate(glm.radians(self.i), glm.vec3(1, 0, 0))
+        ry = glm.rotate(glm.radians(self.i), glm.vec3(0, 1, 0))
+        rz = glm.rotate(glm.radians(self.i), glm.vec3(0, 0, 1))
+        self.scene.matrix = rz * ry * rx
+
+        self.fbo.use()
+        self.wnd.ctx.clear(37/255, 37/255, 38/255)
+        self.wnd.ctx.enable_only(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
+
+        self.scene.draw(
+            projection_matrix=self.proj,
+            camera_matrix=self.camera,
+            time=0.0,
         )
 
-        # mettre à jour la pose
-        #self.scene.set_pose(self.node, pose)
-        color, depth = self.renderer.render(self.scene)
+        raw = self.fbo.color_attachments[0].read()
+        img = np.frombuffer(raw, dtype="u1").reshape(self.config['resolution']['height'], self.config['resolution']['width'], 3)
 
         #frame = cv2.resize(frame, (width, height))
-        frame_1d = color.ravel()  # flatten camera data to a 1d structure
+        frame_1d = np.flipud(img).ravel()  # flatten camera data to a 1d structure
         frame_float = np.asarray(frame_1d, dtype='f')  # change data type to float
         data = np.true_divide(frame_float, 255)  # normalize pixels
 
