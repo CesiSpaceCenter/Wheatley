d5cc45c215b830e66beb7ecd5654cb6fed199e52
Enzo PUIROUX <enzo.puiroux@hypr-space.com>
2026-04-29 17:33:00 +0200

    wip

--- plugins/widgets/status_table/__init__.py
+++ plugins/widgets/status_table/__init__.py
@@ -9,11 +9,12 @@ class StatusTableWidget(BaseWidget):
     name = 'Status table'
 
     config_definition = {
-        'cols': config_types.Int(default=3),
         '"expression" is a piece of python code that should return 0=Gray, 1=Green, 2=Yellow, 3=Red': config_types.Text(),
         'The "data" dict is available to access data values': config_types.Text(),
         'items': config_types.List(config_types.Group({
                 'expression': config_types.Str({'multiline': True}),
+                'x': config_types.Int(),
+                'y': config_types.Int(),
                 'label': config_types.Str()
             })
         ),
@@ -32,21 +33,26 @@ class StatusTableWidget(BaseWidget):
 
     def __init__(self, *args):
         super(StatusTableWidget, self).__init__(*args)
-        rows = ceil(len(self.config['items'])/self.config['cols'])
-        print(rows, self.config['cols'])
+        if len(self.config['items']) == 0:
+            return
+        cols = len(set(item['x'] for item in self.config['items']))
+        rows = len(set(item['y'] for item in self.config['items']))
+        print(rows, cols)
+
+
 
         with dpg.table(parent=self.window, header_row=False, borders_innerV=True, borders_innerH=True) as table:
-            for i in range(self.config['cols']):
+            for i in range(cols):
                 dpg.add_table_column()
-            index = 0
             for i in range(rows):
                 with dpg.table_row():
-                    for j in range(self.config['cols']):
+                    for j in range(cols):
                         with dpg.table_cell():
-                            if index < len(self.config['items']):
-                                dpg.highlight_table_cell(table, i, j, [*self.colors[index], 100])
-                                dpg.add_text(self.config['items'][index]['label'])
-                                index += 1
+                            pass
+                            #if index < len(self.config['items']):
+            for i, item in enumerate(self.config['items']):
+                dpg.highlight_table_cell(table, item['x']+1, item['y']+1, [*self.colors[i], 100])
+                dpg.add_text(item['label'])
 
 
     def render(self):
