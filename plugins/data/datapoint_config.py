b9892838579ff720531ba4ffb95be2ad18035761
Enzo PUIROUX <enzo.puiroux@mail.com>
2026-06-29 17:59:33 +0200

    feat: datapoint optimisation, typing, cleanup

--- plugins/data/datapoint_config.py
+++ plugins/data/datapoint_config.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass
-from typing import Any, Type
+from typing import Any, Type, overload
 from collections import deque
 import time
-
-from PIL.SpiderImagePlugin import isInt
 
 
 class DataPoint:
@@ -15,29 +12,55 @@ class DataPoint:
         self.name = name
         self.type = type
         self.unit = unit
-        self.history = deque(maxlen=150)
+        self.min = 0
+        self.max = 0
+        self.avg = 0
+        self.history = deque(maxlen=3000)
 
     def append(self, value: Any | list[Any]):
-        if isinstance(value, list):
+        if isinstance(value, list):  # multiple values
             t = time.monotonic()
             for v in value:
                 self.history.append((t, v))
         else:
             self.history.append((time.monotonic(), value))
 
+        l =  [d[1] for d in self.history]
+        self.max = max(l)
+        self.min = min(l)
+        self.avg = sum(l)/len(l)
+
     def __repr__(self):
         return f'<DataPoint {self.name}>'
 
-    @staticmethod
-    def closest_datapoint_timestamp(d: list[tuple[float, Any]], target: Any) -> int:
-        for index, (ts, val) in enumerate(d[:-1]):
+    @property
+    def has_data(self) -> bool:
+        """ returns True is at least one datapoint is present """
+        return len(self.history) > 0
+
+    def _closest_datapoint_timestamp(self, target_t: Any) -> int:
+        """ returns the index of the point in self.history which timestamp is the closest to the specified target_t """
+        for index, (ts, val) in enumerate(self._history_slice(0, -1)):
             # is the next item farther away from the current one?
-            if abs(target - d[index+1][1]) > abs(target - ts):
+            if abs(target_t - self.history[index + 1][1]) > abs(target_t - ts):
                 # if yes, no other item will be closer than the current one
                 return index
         return -1  # target is above the last value, so the last value is the closest
 
-    def __getitem__(self, t: float | int | slice) -> tuple[float, Any] | list[tuple[float, Any]] | None:
+    def _history_slice(self, start: int, stop: int) -> list:
+        """ slices self.history deque """
+        if stop < 0:  # negative index, start from the end
+            stop = len(self.history)+stop+1
+        return [self.history[i] for i in range(start, stop)]
+
+    @overload
+    def __getitem__(self, index: float | int) -> tuple[float, Any]:
+        ...
+    @overload
+    def __getitem__(self, indexes: slice) -> list[tuple[float, Any]]:
+        ...
+
+    def __getitem__(self, t: float | int | slice) -> tuple[float, Any] | list[tuple[float, Any]]:
         now = time.monotonic()
         if isinstance(t, int) or isinstance(t, float):
             if t == 0:  # shortcut for last available value
@@ -57,32 +80,11 @@ class DataPoint:
             raise TypeError(f'Invalid index type: {type(t)}, expected float, int or slice')
 
         if not self.history:
-            print(self, 'no data')
-            return None
-        print(max_time)
+            raise ValueError(f'no data available on {self}')
         if single_value:
-            return self.history[self.closest_datapoint_timestamp(self.history, max_time)]
+            idx = self._closest_datapoint_timestamp(max_time)
+            return self.history[idx]
 
-        #min_index = self.closest_datapoint_timestamp(self.history, min_time)
-        #max_index = self.closest_datapoint_timestamp(self.history, max_time)
-        #return self.history[min_index:max_index]
-
-
-        ret = []
-        for t, val in reversed(self.history):
-            if min_time <= t and not t <= max_time:  # we have not reached the range yet
-                continue
-            elif not min_time <= t and t <= max_time:  # we have passed the range, we can abort
-                break
-            elif not min_time <= t and not t <= max_time:  # what??????
-                raise Exception('what the fuck')
-            else:  # we are in the range
-                ret.append((t-now, val))
-                if single_value:
-                    return ret[0]
-        if ret:
-            return list(reversed(ret))
-        if single_value:
-            return self.history[0]  # return oldest value
-        else:
-            return None
+        min_index = self._closest_datapoint_timestamp(min_time)
+        max_index = self._closest_datapoint_timestamp(max_time)
+        return self._history_slice(min_index, max_index)
